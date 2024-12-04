43ff147dfea6614f9a5022915749fec35fca8bc5
Mattia Procopio <promat85@gmail.com>
2024-12-04 11:16:37 +0100

    Finish off day2

--- 2024/ex-2/src/main.rs
+++ 2024/ex-2/src/main.rs
@@ -4,65 +4,81 @@ fn read_file() -> String {
     read_to_string("file.txt").unwrap()
 }
 
+fn is_report_safe(line: &[u32]) -> bool {
+    let mut asc = false;
+    let mut desc = false;
+
+    // Iterate for the next 2 elements of the slice
+    for couple in line.windows(2) {
+        if couple[0] < couple[1] {
+            asc = true;
+	} else if couple[0] > couple[1] {
+	    desc = true;
+	} else {
+            return false;
+	}
+
+        if couple[0].abs_diff(couple[1]) > 3 {
+            return false;
+        }
+    }
+
+    if asc && desc {
+        return false;
+    }
+
+    true
+}
+
 fn first_part() -> u32 {
     let mut safe_reports: u32 = 0;
 
-    for line in read_file().lines() {
-        let mut numbers: Vec<u32> = Vec::new();
-        let parts: Vec<&str> = line.split(" ").collect();
+    let lines: Vec<Vec<u32>> = read_file().lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|num| num.parse::<u32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect();
 
-        if parts.len() <= 2 {
-            safe_reports += 1
-        }
-
-        for part in parts {
-            numbers.push(part.parse::<u32>().unwrap());
-        }
-
-	let mut iter_num = numbers.into_iter();
-	let mut previous = iter_num.next().unwrap();
-	let mut actual = iter_num.next().unwrap();
-	let mut fail = false;
-	
-	if actual < previous && previous - actual <= 3 {
-	    for n in iter_num {
-		if n > actual || (n < actual && actual - n > 3) || actual == n {
-		    fail = true;
-		    break;
-		} else {
-		    previous = actual;
-		    actual = n;
-		}
-	    }
-
-	    if !fail {
-		safe_reports += 1
-	    }
-	    
-	} else if actual > previous && actual - previous <= 3 {
-	    for n in iter_num {
-		if n < actual || (n > actual && n - actual > 3) || actual == n {
-		    fail = true;
-		    break;
-		} else {
-		    previous = actual;
-		    actual = n;
-		}
-	    }
-
-	    if !fail {
-		safe_reports += 1
-	    }
-	} else {
-	     fail = true;
+    for line in &lines {
+	if is_report_safe(&line) {
+	    safe_reports += 1;
 	}
     }
+    
 
     safe_reports
 }
 
 fn second_part() -> u32 {
-    0
+    let mut safe_reports: u32 = 0;
+
+    let lines: Vec<Vec<u32>> = read_file().lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|num| num.parse::<u32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+	.collect();
+
+    for line in &lines {
+	if is_report_safe(&line) {
+            safe_reports += 1;
+        } else {
+	    for i in 0..line.len() {
+		let (left, right) = line.split_at(i);
+		let slice = [left, &right[1..]].concat();
+		if is_report_safe(&slice) {
+		    safe_reports += 1;
+                    break;
+		}
+	    }
+        }
+    }
+
+
+    safe_reports
 }
 
 fn main() {
